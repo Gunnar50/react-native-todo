3662ec3faf48385462cf8734eb8d3245d62b4d0b
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-09-17 14:25:26 +0100

    add and remove todo

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from "./components/Task";
 
 export default function App() {
+	const [task, setTask] = useState("");
+	const [taskItems, setTaskItems] = useState([]);
+
+	const handleAddTask = () => {
+		Keyboard.dismiss();
+		setTaskItems([...taskItems, task]);
+		setTask("");
+	}
+
+	const completeTask = (idx) => {
+		const itemsCopy = [...taskItems];
+		itemsCopy.splice(idx, 1);
+		setTaskItems(itemsCopy);
+	}
+
 	return (
 		<View style={styles.container}>
 			{/* Today Tasks */}
@@ -12,10 +27,35 @@ export default function App() {
 
 				<View style={styles.items}>
 					{/* Tasks will go here */}
-					<Task/>
+					{taskItems.map((item, idx) => {
+						return (
+
+							<TouchableOpacity onPress={() => completeTask(idx)}>
+								<Task key={idx} text={item}/>
+							</TouchableOpacity>	
+
+						)
+					})}
+					
 
 				</View>
 			</View>
+
+			{/* Write a task */}
+			<KeyboardAvoidingView
+				behavior={Platform.OS === 'ios' ? "padding" : "height"}
+				style={styles.writeTaskWrapper}
+			>
+				<TextInput style={styles.input} placeholder="Write a Task" value={task} onChangeText={text => setTask(text)}></TextInput>
+
+				<TouchableOpacity onPress={handleAddTask}>
+					<View style={styles.addWrapper}>
+						<Text style={styles.addText}>+</Text>
+					</View>
+				</TouchableOpacity>
+
+			</KeyboardAvoidingView>
+
 
 		</View>
 	);
@@ -24,7 +64,7 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#f4f4f4',
+		backgroundColor: '#fefefe',
 	},
 	taskWrapper: {
 		paddingTop: 80,
@@ -34,5 +74,37 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		fontWeight: "bold"
 	},
-	items: {}
+	items: {
+		marginTop: 20,
+	},
+	writeTaskWrapper: {
+		position: "absolute",
+		bottom: 60,
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center",
+		width: "100%",
+	},
+	input: {
+		paddingHorizontal: 15,
+		paddingVertical: 10,
+		backgroundColor: "#eee",
+		borderRadius: 50,
+		width: 250,
+		borderColor: "#c0c0c0",
+		borderWidth: 1,
+	},
+	addWrapper: {
+		width: 60,
+		height: 60,
+		backgroundColor: "#eee",
+		borderColor: "#c0c0c0",
+		borderWidth: 2,
+		borderRadius: 50,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	addText: {
+		fontSize: 30
+	},
 });
--- components/Task.js
+++ components/Task.js
@@ -1,15 +1,55 @@
 import React from 'react'
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
-export default function Task() {
+export default function Task(props) {
 
 	return (
-		<View>
-			<Text>This is a task</Text>
+		<View style={styles.item}>
+			<View style={styles.itemLeft}>
+				<TouchableOpacity style={styles.square}></TouchableOpacity>
+				<Text style={styles.itemText}>{props.text}</Text>
+			</View>
+
+			<View style={styles.circular}></View>
+
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	
+	item: {
+		backgroundColor: "lightblue",
+		padding: 10,
+		marginBottom: 10,
+		borderRadius: 10,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+	},
+	itemLeft: {
+		flexDirection: "row",
+		alignItems: "center",
+		flexWrap: "wrap",
+	},
+	square: {
+		backgroundColor: "#aaaa99",
+		opacity: 0.4,
+		width: 20,
+		height: 20,
+		borderRadius: 5,
+		marginRight: 15,
+		
+	},
+	itemText: {
+		maxWidth: "80%",
+		
+	},
+	circular: {
+		width: 12,
+		height: 12,
+		borderColor: "#aaaa99",
+		borderWidth: 2,
+		borderRadius: 5,
+
+	},
 })
